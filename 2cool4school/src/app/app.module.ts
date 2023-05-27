26b466efe75860babb39ef8319f768a6244d2176
nielshns <niels.hunsche@gmail.com>
2023-05-27 15:16:28 +0200

    added navbar

--- 2cool4school/src/app/app.module.ts
+++ 2cool4school/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { XyzComponent } from './xyz/xyz.component';
 import { LoginComponent } from './xyz/login/login.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     XyzComponent,
-    LoginComponent
+    LoginComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
