2ffc1ca0b5bbc14b1e7ffede0e95c68b8cf39711
nielshns <niels.hunsche@gmail.com>
2023-05-27 14:24:03 +0200

    added login component

--- 2cool4school/src/app/app.module.ts
+++ 2cool4school/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { XyzComponent } from './xyz/xyz.component';
+import { LoginComponent } from './xyz/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    XyzComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
